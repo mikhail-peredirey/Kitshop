3a9d2fd13248b56957e01d385e1003f0ab4f6c56
Mikhail Peredirey <mikhail.peredirey@gmail.com>
2015-07-03 02:49:42 +0600

    ProductBuilder make

---
+++ src/main/java/com/epam/peredirey/model/ProductBuilder.java
@@ -0,0 +1,30 @@
+package com.epam.peredirey.model;
+
+/**
+ * Created by mikhail.peredirey on 03.07.2015.
+ */
+public class ProductBuilder {
+
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Integer getPorts() {
+        return ports;
+    }
+
+    public String getFirmware() {
+        return firmware;
+    }
+
+    public String getFeature() {
+        return feature;
+    }
+
+
+}
